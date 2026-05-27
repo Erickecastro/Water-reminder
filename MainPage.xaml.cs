d3f3a41263fa94668c4cbdb4f1d50a5610afea91
Ericke Castro <eerickevh@gmail.com>
2026-05-27 14:00:26 -0400

    MainPage fix

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,15 +5,25 @@ namespace Water_reminder;
 
 public partial class MainPage : ContentPage
 {
-	private readonly MainViewModel _vm;
+    private readonly MainViewModel _vm;
 
-	public MainPage(MainViewModel viewModel)
-	{
-		InitializeComponent();
-		_vm = viewModel;
-		BindingContext = _vm;
+    public MainPage() : this(GetViewModel())
+    {
+    }
 
-		_ = Task.Run(async () => await _vm.LoadDataAsync());
-	}
+    public MainPage(MainViewModel viewModel)
+    {
+        InitializeComponent();
+        _vm = viewModel ?? throw new InvalidOperationException("MainViewModel não pôde ser criado.");
+        BindingContext = _vm;
+
+        _ = Task.Run(async () => await _vm.LoadDataAsync());
+    }
+
+    private static MainViewModel GetViewModel()
+    {
+        return IPlatformApplication.Current?.Services.GetService<MainViewModel>()
+            ?? throw new InvalidOperationException("Serviço MainViewModel não está registrado.");
+    }
 }
 
